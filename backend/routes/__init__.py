d3d23927cf79da693a93507ff42015d3cfee1c1b
weiso131 <blackfire106.1231@gmail.com>
2025-01-25 22:43:16 +0800

    Use beanie on user

--- backend/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import auth, user, trick, teacher
-
-def init_app(db):
-    auth.init_app(db)
-    user.init_app(db)
-    trick.init_app(db)
-    teacher.init_app(db)
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -3,54 +3,43 @@ import bcrypt
 
 
 from .utils.jwt import *
-from schemas.user import UserUpdate
+from schemas.user import UserUpdate, User
 from exception import *
 
-user_collection = None
-
-
-def init_app(db):
-    global user_collection
-    user_collection = db['user']
 
 async def check_user_exist(uid):
-    find = await user_collection.find_one({"uid": uid})
+    find = await User.find_one(User.uid == uid)
     if not find:
         raise USER_NOT_EXISTS
+    return find
 
 router = APIRouter()
 
 @router.get('/get')
 async def get_data(data=UserDepend):
     uid = data['uid']
-    user = await user_collection.find_one({"uid": uid})
+    user = await User.find_one(User.uid == uid)
     if not user:
         raise USER_NOT_EXISTS
-    user.pop("_id", None)
-    return user
+    user_dict = user.dict()
+    user_dict.pop("id", None)
+    return user_dict
 
 @router.post('/update')
 async def update(new_data: UserUpdate, data=UserDepend):
     uid = data["uid"]
-    check_user_exist(uid)
+    user = await check_user_exist(uid)
     
-    update_data = {k: v for k, v in new_data.model_dump().items() if v is not None}
-
-    await user_collection.update_one(
-        {"uid": uid},
-        {"$set": update_data}
-    )
+    await user.set(new_data.model_dump(exclude_none=True))
 
     return {"status_code": True}
 
 @router.delete('/delete')
 async def delete(password: str, data=UserDepend):
     uid = data['uid']
-    user = await user_collection.find_one({"uid": uid})
-    if not user:
-        raise USER_NOT_EXISTS
-    if not bcrypt.checkpw(password.encode(), user["password"].encode("utf-8")):
+    user = await check_user_exist(uid)
+    if not bcrypt.checkpw(password.encode(), user.password.encode("utf-8")):
         return WRONG_EMAIL_OR_PASSWORD
-    await user_collection.delete_one({"uid": uid})
+    await user.delete()
     return {"status_code": True}
 
